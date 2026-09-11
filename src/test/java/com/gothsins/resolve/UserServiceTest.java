51cc729ab6ffd230d873d4a4b1e84d800de88615
gothsins <gui.starkteam@gmail.com>
2026-09-11 00:44:26 -0300

    Add unit tests for UserService to validate user creation and retrieval

--- src/test/java/com/gothsins/resolve/UserServiceTest.java
+++ src/test/java/com/gothsins/resolve/UserServiceTest.java
@@ -1,14 +1,18 @@
 package com.gothsins.resolve;
 
+import com.gothsins.resolve.dto.UserRequestDTO;
 import com.gothsins.resolve.dto.UserResponseDTO;
 import com.gothsins.resolve.entity.User;
 import com.gothsins.resolve.exception.ResourceNotFoundException;
 import com.gothsins.resolve.repository.UserRepository;
+import com.gothsins.resolve.service.MetricsService;
 import com.gothsins.resolve.service.UserService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
@@ -22,6 +26,9 @@ class UserServiceTest {
 
     @Mock
     private UserRepository userRepository;
+
+    @Mock
+    private MetricsService metricsService;
 
     @InjectMocks
     private UserService userService;
@@ -61,5 +68,64 @@ class UserServiceTest {
         );
 
         verify(userRepository).findById(userId);
+    }
+
+    @Test
+    void testCreateUserRequestDTO() {
+
+        UserRequestDTO dto = new UserRequestDTO();
+
+        dto.setName("Guiverme");
+        dto.setEmail("guiverme@email.com");
+        dto.setPassword("p12345678");
+
+        User savedUser = new User();
+        savedUser.setId(1L);
+        savedUser.setName(dto.getName());
+        savedUser.setEmail(dto.getEmail());
+        savedUser.setPassword(dto.getPassword());
+
+        Mockito.when(userRepository.save(Mockito.any(User.class))).thenReturn(savedUser);
+
+        UserResponseDTO result = userService.create(dto);
+
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(1L, result.getId());
+        Assertions.assertEquals("Guiverme", result.getName());
+        Assertions.assertEquals("guiverme@email.com", result.getEmail());
+
+        Mockito.verify(metricsService, Mockito.times(1)).incrementUserRegistered();
+    }
+
+    @Test
+    void testfindById() {
+        Long userId = 1L;
+
+        User user = new User();
+        user.setId(userId);
+        user.setName("Guiverme");
+        user.setEmail("guiverme@email.com");
+
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+
+        UserResponseDTO result = userService.findById(1L);
+
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(1L, result.getId());
+        Assertions.assertEquals("Guiverme", result.getName());
+        Assertions.assertEquals("guiverme@email.com", result.getEmail());
+    }
+
+    @Test
+    void testFindById_UserNotFound_ThrowsException() {
+        Long nonExistingId = 999L;
+
+        Mockito.when(userRepository.findById(nonExistingId)).thenReturn(Optional.empty());
+
+        RuntimeException exception = Assertions.assertThrows(RuntimeException.class, () -> {
+            userService.findById(nonExistingId);
+        });
+
+        Assertions.assertEquals("User not found", exception.getMessage());
     }
 }
